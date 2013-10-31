e4632a8e8c8f240ba9c1b66ffe18b96aa9df3549
yarlen <yarlenvas@gmail.com>
2013-10-30 21:59:58 -0500

    - Se crea aplicación de prueba, que usa WebKitDotNet para embeber webkit dentro de una aplicación de windows forms

---
+++ labs/WebKitLab/TestAPP/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TestAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.SuspendLayout();
+            // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.AllowAnimatedImages = true;
+            this.webKitBrowser1.AllowFileAccessFromFileURLs = false;
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.ClientCertificate = null;
+            this.webKitBrowser1.CookieAcceptPolicy = WebKit.CookieAcceptPolicy.Always;
+            this.webKitBrowser1.IsLocalStorageEnabled = true;
+            this.webKitBrowser1.IsScriptingEnabled = true;
+            this.webKitBrowser1.IsWebBrowserContextMenuEnabled = true;
+            this.webKitBrowser1.Location = new System.Drawing.Point(23, 12);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(726, 452);
+            this.webKitBrowser1.TabIndex = 0;
+            this.webKitBrowser1.Url = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 511);
+            this.Controls.Add(this.webKitBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WebKit.WebKitBrowser webKitBrowser1;
+    }
+}
+
---
+++ labs/WebKitLab/TestAPP/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.webKitBrowser1.Navigate("http://www.google.com");
+        }
+    }
+}
